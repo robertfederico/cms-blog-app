0777ae64ab07b97ce92625111662ad98b019add6
robertfederico <robertbfederico@gmail.com>
2020-03-04 13:39:08 +0800

    update category function

--- admin/functions.php
+++ admin/functions.php
@@ -18,12 +18,17 @@ function insert_categories()
     if (isset($_POST['submit'])) {
         $category = $_POST['cat_title'];
 
+        $cat_image = $_FILES['cat_image']['name'];
+        $cat_image_temp = $_FILES['cat_image']['tmp_name'];
+
+        move_uploaded_file($cat_image_temp, "../images/$cat_image");
+
         if (empty($category)) {
             echo '<p class="text-danger m-0 font-italic">add a category</p>';
         } else {
 
-            $query = "INSERT INTO categories(category_title)";
-            $query .= "VALUE('{$category}')";
+            $query = "INSERT INTO categories(category_title,cat_image)";
+            $query .= "VALUE('{$category}','{$cat_image}')";
 
             $insert = mysqli_query($conn, $query);
 
@@ -43,12 +48,15 @@ function display_categories()
 
     $cat_row_id = 1;
     while ($row = mysqli_fetch_assoc($select_categories)) {
-        $cat_title = $row['category_title'];
         $cat_id = $row['category_id'];
+        $cat_title = $row['category_title'];
+        $cat_image = $row['cat_image'];
+
         echo "
         <tr>
             <td>{$cat_row_id}</td>
             <td>{$cat_title}</td>
+            <td><img class='img-fluid' src='../images/$cat_image'></td>
             <td>
                 <a href='categories.php?delete={$cat_id}' class='btn btn-danger btn-sm rounded-circle'>
                     <i class='fas fa-trash'></i>
@@ -58,6 +66,7 @@ function display_categories()
                 </a>
             </td>
         </tr>";
+
         $cat_row_id++;
     }
 }
@@ -79,11 +88,27 @@ function delete_category()
 function update_category()
 {
     global $conn;
+
     if (isset($_POST['update'])) {
+
         $update_category_id = $_POST['update_category_id'];
         $update_category = $_POST['update_category'];
 
-        $query_update = "UPDATE categories  SET category_title ='{$update_category}'  WHERE category_id= {$update_category_id}";
+        $update_cat_image = $_FILES['update_cat_image']['name'];
+        $post_image_temp = $_FILES['update_cat_image']['tmp_name'];
+
+        if (empty($update_cat_image)) {
+            $select_old_image = "SELECT * FROM categories WHERE category_id = {$update_category_id}";
+            $result = mysqli_query($conn, $select_old_image);
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                $update_cat_image = $row['cat_image'];
+            }
+        }
+
+        move_uploaded_file($post_image_temp, "../images/$update_cat_image");
+
+        $query_update = "UPDATE categories  SET category_title ='{$update_category}', cat_image ='{$update_cat_image}'  WHERE category_id= {$update_category_id}";
         $update = mysqli_query($conn, $query_update);
         if ($update) {
             header("Location: categories.php");
