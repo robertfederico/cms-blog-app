6fcbbbea2b75cfcda2469515fc87c793172c69ac
robertfederico <robertbfederico@gmail.com>
2020-03-06 00:18:26 +0800

    update view posts page

--- admin/view-posts.php
+++ admin/view-posts.php
@@ -8,28 +8,43 @@ include 'includes/header.php';
     <!-- sidebar-wrapper  -->
     <main class="page-content">
         <div class="container-fluid">
-            <h2>Posts</h2>
-            <hr>
             <div class="card shadow">
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <?php
-                        if (isset($_GET['source'])) {
-                            $source = $_GET['source'];
-                        } else {
-                            $source = '';
-                        }
-                        switch ($source) {
-                            case 'add_post';
-                                include 'includes/add_posts.php';
-                                break;
-                            default:
-                                include 'includes/view_all_posts.php';
-                                break;
-                        }
+                <div class="card-header bg-transparent">
+                    <?php
+                    if (isset($_GET['source'])) {
+                        $source = $_GET['source'];
+                    } else {
+                        $source = '';
+                    }
 
-                        ?>
-                    </div>
+                    if ($source == 'add_post') {
+
+                        echo '<h2>Add Post</h2>';
+                    } else if ($source == 'edit_post') {
+
+                        echo '<h2>Edit Post</h2>';
+                    } else {
+
+                        echo '<h2>Post</h2>';
+                    }
+                    ?>
+                </div>
+                <div class="card-body">
+                    <?php
+                    switch ($source) {
+                        case 'add_post';
+                            include 'includes/add_posts.php';
+                            break;
+                        case 'edit_post';
+                            include 'includes/edit_post.php';
+                            break;
+                        default:
+                            echo ' <div class="table-responsive">';
+                            include 'includes/view_all_posts.php';
+                            echo '</div>';
+                            break;
+                    }
+                    ?>
                 </div>
             </div>
         </div>
