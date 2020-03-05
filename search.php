7fc561b0a4962ae3be402731faee923dcf1b6e1f
robertfederico <robertbfederico@gmail.com>
2020-03-06 00:05:33 +0800

    improve search page design

--- search.php
+++ search.php
@@ -3,11 +3,8 @@
 <?php include "includes/navbar.php"; ?>
 
 <!-- Page Content -->
-<div class="container">
-
+<div class="container mt-5">
     <div class="row">
-
-        <!-- Blog Entries Column -->
         <div class="col-md-8">
             <?php
 
@@ -31,32 +28,26 @@
                 } else {
 
                     while ($row = mysqli_fetch_assoc($search_query)) {
+                        $post_id = $row['posts_id'];
                         $post_title = $row['post_title'];
                         $post_author = $row['post_author'];
                         $post_date = $row['post_date'];
                         $post_image = $row['post_image'];
                         $post_content = $row['post_content'];
             ?>
-
-            <h1 class="page-header">
-                Page Heading
-                <small>Secondary Text</small>
-            </h1>
-            <!-- First Blog Post -->
-            <h2>
-                <a href="#"><?php echo $post_title ?></a>
-            </h2>
-            <p class="lead">
-                by <a href="index.php"><?php echo $post_author ?></a>
-            </p>
-            <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
-            <hr>
-            <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-            <hr>
-            <p><?php echo $post_content ?></p>
-            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-            <hr>
+            <div class="card mb-5 open-post">
+                <div class="img-container">
+                    <a href="post.php?p_id=<?php echo $post_id; ?>">
+                        <img class="img-card" src="images/<?php echo $post_image;  ?>" />
+                        <div class="desc-box">
+                            <h3>
+                                <?php echo $post_title; ?>
+                            </h3>
+                            <p class="author"><?php echo $post_author; ?></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
 
             <?php }
                 }
@@ -64,9 +55,5 @@
 
             ?>
         </div>
-        <!-- Blog Sidebar Widgets Column -->
-        <?php include "includes/sidebar.php"; ?>
     </div>
-    <!-- /.row -->
-    <hr>
     <?php include "includes/footer.php"; ?>
