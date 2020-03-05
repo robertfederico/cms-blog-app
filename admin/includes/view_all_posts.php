84f87b7996f2d9883562167305c82fe023537882
robertfederico <robertbfederico@gmail.com>
2020-03-06 00:17:00 +0800

    update view post page

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,3 +1,5 @@
+<!-- Fix this tomorrow -->
+
 <table id="post_table" class="table table-hover text-nowrap">
     <thead>
         <tr>
@@ -15,9 +17,10 @@
     </thead>
     <tbody>
         <?php
+        $posts_row_id = 1;
+
         $query = "SELECT * FROM posts";
         $select_posts = mysqli_query($conn, $query);
-        $posts_row_id = 1;
         while ($row = mysqli_fetch_assoc($select_posts)) {
             $posts_id = $row['posts_id'];
             $post_cat_id = $row['post_cat_id'];
@@ -30,12 +33,20 @@
             $post_tag = $row['post_tag'];
             $post_comment_count = $row['post_comment_count'];
             $post_status = $row['post_status'];
+
+            $query_category = "SELECT * FROM categories WHERE category_id ={$post_cat_id}";
+            $fetch_result  = mysqli_query($conn, $query_category);
+
+            while ($row = mysqli_fetch_assoc($fetch_result)) {
+                $category_title = $row['category_title'];
+            }
+
             echo "
                 <tr>
                     <td>{$posts_row_id}</td>
                     <td>{$post_author}</td>
                     <td>{$post_title}</td>
-                    <td>{$post_cat_id}</td>
+                    <td>{$category_title}</td>
                     <td>{$post_status}</td>
                     <td><img class='img-fluid' src='../images/$post_image'></td>
                     <td>{$post_tag}</td>
@@ -45,15 +56,7 @@
                         <a href='view-posts.php?delete={$posts_id}' class='btn btn-danger btn-sm rounded-circle'>
                             <i class='fas fa-trash'></i>
                         </a>
-                        <a href='#' data-id='{$posts_id}' 
-                        data-post_author='{$post_author}' 
-                        data-post_title='{$post_title}' 
-                        data-post_cat_id='{$post_cat_id}' 
-                        data-post_status='{$post_status}' 
-                        data-post_tag='{$post_tag}' 
-                        data-post_image='{$post_image}' 
-                        data-post_content='{$data}' 
-                        class='btn btn-success btn-sm edit_posts rounded-circle'>
+                        <a href='view-posts.php?source=edit_post&p_id={$posts_id}' class='btn btn-success btn-sm edit_posts rounded-circle'>
                             <i class='fas fa-edit'></i>
                         </a>
                     </td>
@@ -63,133 +66,6 @@
         ?>
     </tbody>
 </table>
-
-<!-- Modal -->
-<div class="modal fade" id="edit_posts" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Update Posts</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="" method="POST" enctype="multipart/form-data">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="cat_title">Post Title</label>
-                                <input name="update_post_title" id="update_post_title" type="text" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="cat_title">Post Category Id</label>
-                                <select name="update_post_category_id" id="update_post_category_id"
-                                    class="form-control">
-                                    <?php
-                                    $query = "SELECT * FROM categories";
-                                    $category_query = mysqli_query($conn, $query);
-                                    echo "<option value=''>Select Category</option>";
-                                    while ($row = mysqli_fetch_assoc($category_query)) {
-                                        $category_id = $row['category_id'];
-                                        $category_title = $row['category_title'];
-                                        echo "<option value='{$category_id}'>{$category_title}</option>";
-                                    }
-                                    ?>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="cat_title">Post Author</label>
-                                <input name="update_post_author" id="update_post_author" type="text"
-                                    class="form-control">
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="cat_title">Post Status</label>
-                                <input name="update_post_status" id="update_post_status" type="text"
-                                    class="form-control">
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <label for="cat_title">Post Image</label>
-                                <input name="update_post_image" id="update_post_image" type="file" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <label for="cat_title">Post Tags</label>
-                                <input name="update_post_tags" id="update_post_tags" type="text" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <label for="">Post Content</label>
-                                <textarea name="update_post_content" id="update_post_content"
-                                    class="form-control"></textarea>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <input type="hidden" name="post_id" id="post_id">
-                        <button class="btn btn-primary" name="update_post" id="update_post" type="submit">Update
-                            Post</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-
-<?php
-if (isset($_POST['update_post'])) {
-
-    $posts_id = $_POST['post_id'];
-
-    $update_post_title = $_POST['update_post_title'];
-    $update_post_category_id = $_POST['update_post_category_id'];
-    $update_post_status = $_POST['update_post_status'];
-
-    $post_image = $_FILES['update_post_image']['name'];
-    $post_image_temp = $_FILES['update_post_image']['tmp_name'];
-
-    $update_post_tags = $_POST['update_post_tags'];
-    $update_post_author = $_POST['update_post_author'];
-    $update_post_content = $_POST['update_post_content'];
-
-    if (empty($post_image)) {
-        $select_old_image = "SELECT * FROM posts WHERE posts_id ={$posts_id}";
-        $result = mysqli_query($conn, $select_old_image);
-        while ($row = mysqli_fetch_assoc($result)) {
-            $post_image = $row['post_image'];
-        }
-    }
-
-    echo $posts_id;
-
-    move_uploaded_file($post_image_temp, "../images/$post_image");
-
-    $update_query = "UPDATE posts SET ";
-    $update_query .= "post_cat_id= {$update_post_category_id}, ";
-    $update_query .= "post_title = '{$update_post_title}', ";
-    $update_query .= "post_author = '{$update_post_author}', ";
-    $update_query .= "post_image = '{$post_image}', ";
-    $update_query .= "post_content='{$update_post_content}', ";
-    $update_query .= "post_tag= '{$update_post_tags}', ";
-    $update_query .= "post_status= '{$update_post_status}' ";
-    $update_query .= "WHERE posts_id = {$posts_id}";
-
-    $update_posts = mysqli_query($conn, $update_query);
-    confirm_query($update_posts);
-    header("Location: view-posts.php");
-}
-?>
 
 <?php
 if (isset($_GET['delete'])) {
