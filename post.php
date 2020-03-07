4ef8287148d416ffff8a12771a273c578e9a08cd
robertfederico <robertbfederico@gmail.com>
2020-03-07 13:29:25 +0800

    add comments

--- post.php
+++ post.php
@@ -46,11 +46,32 @@
             <div class="text-justify mb-5"><?php echo $post_content; ?>
 
             </div>
-            <div class="card shadow-sm comment-section border-0">
+            <div class="card shadow-sm comment-section border-0 mb-5">
                 <?php
-                    if (isset($_POST['submit_comment']))
+                    if (isset($_POST['submit_comment'])) {
 
-                        echo  $_POST['comment_author'];
+                        $post_id = $_GET['p_id'];
+                        $comment_author = $_POST['comment_author'];
+                        $comment_email = $_POST['comment_email'];
+                        $comment = mysqli_real_escape_string($conn, $_POST['comment']);
+                        $post_date = date('Y-m-d');
+
+                        $insert_query = "INSERT INTO comments(comment_post_id,comment_author,
+                        comment_email,comment_content,comment_status,comment_date)
+                        VALUES($post_id,'{$comment_author}', '{$comment_email}', 
+                        '{$comment}','approved','{$post_date}')";
+
+                        $insert_query_comments = mysqli_query($conn, $insert_query);
+
+                        if (!$insert_query_comments) {
+                            die('Failed' . mysqli_error($conn));
+                        } else {
+                            $query_update_post = "UPDATE posts 
+                            SET post_comment_count = post_comment_count + 1 WHERE posts_id = $post_id";
+                            $update_post = mysqli_query($conn, $query_update_post);
+                            echo "<script>alert('Success');</script>";
+                        }
+                    }
 
                     ?>
                 <div class="card-header bg-transparent border-0 pt-5">
@@ -75,11 +96,52 @@
                     </form>
                 </div>
             </div>
+
+
+            <?php
+                $post_id = $_GET['p_id'];
+                $query_comment = "SELECT * FROM comments WHERE comment_post_id = $post_id 
+                    AND comment_status='approved' ORDER BY comment_id DESC";
+
+                $result = mysqli_query($conn, $query_comment);
+
+                if (!$result) {
+                    die('Failed' . mysqli_error($conn));
+                }
+
+                while ($row = mysqli_fetch_array($result)) {
+                    $comment_id = $row['comment_id'];
+                    $comment_post_id = $row['comment_id'];
+                    $comment_author = $row['comment_author'];
+                    $comment_email = $row['comment_email'];
+                    $comment_content = $row['comment_content'];
+                    $comment_status = $row['comment_status'];
+                    $comment_date  = $row['comment_date'];
+                ?>
+
+            <div class="media mb-3">
+                <a class="mr-3" href="#">
+                    <i class="fas fa-user fa-3x"></i>
+                </a>
+                <div class="media-body">
+                    <h5 class="media-heading"><?php echo  $comment_author; ?>
+                        <small><?php echo  $comment_date; ?></small>
+                    </h5>
+                    <?php echo  $comment_content; ?>
+                </div>
+            </div>
+            <?php
+                }
+                ?>
         </div>
 
         <?php
     }
+
         ?>
-        <?php include 'includes/footer.php'; ?>
+        <div class="mt-5">
+            <?php include 'includes/footer.php'; ?>
+        </div>
+
     </div>
     <!-- /.row -->
