38e0c09320c6c9f21ee7279153c3fda255311cf2
robertfederico <robertbfederico@gmail.com>
2020-03-09 06:03:00 +0800

    add session

--- includes/login.php
+++ includes/login.php
@@ -3,29 +3,26 @@ require 'db_conn.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if (empty(trim($_POST["username"]))) {
-        $username_err = "Please enter username.";
-    } else {
-        $username = trim($_POST["username"]);
-    }
-
-    if (empty(trim($_POST["password"]))) {
-        $password_err = "Please enter your password.";
-    } else {
-        $password = trim($_POST["password"]);
-    }
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
 
     $sql = "SELECT * FROM users WHERE username ='{$username}'";
     $select_posts = mysqli_query($conn, $sql);
 
-    while ($row = mysqli_fetch_assoc($select_posts)) {
-        $user_password = $row['user_password'];
-        $user_name = $row['username'];
+    $row = mysqli_fetch_assoc($select_posts);
+    $user_password = $row['user_password'];
+    $user_name = $row['username'];
 
-        if ($username !== $user_name && $password !== $user_password) {
-            header("Location: ../index.php");
-        } else if ($username == $user_name && $password == $user_password) {
-            header("Location: ../admin/index.php");
-        }
+    if ($username === $user_name && $password === $user_password) {
+        session_start();
+        $_SESSION['user_id'] = $row['user_id'];
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['f_name'] = $row['user_firstname'];
+        $_SESSION['l_name'] = $row['user_lastname'];
+        $_SESSION['user_role'] = $row['user_role'];
+        $_SESSION['user_email'] = $row['user_email'];
+        header("Location: ../admin/index.php");
+    } else {
+        header("Location: ../index.php");
     }
 }
