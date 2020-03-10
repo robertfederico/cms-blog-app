c097e3f5b709638a4b4327361d54a5083d0e222d
robertfederico <robertbfederico@gmail.com>
2020-03-11 00:23:37 +0800

    update header title

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -25,7 +25,7 @@ if (!isset($_SESSION['user_role'])) {
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Simple Sidebar - Start Bootstrap Template</title>
+    <title>CMS | Admin</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
