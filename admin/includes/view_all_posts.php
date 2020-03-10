53ffdb8410fad1b1a835ecc8c6191025406198ae
robertfederico <robertbfederico@gmail.com>
2020-03-11 00:25:09 +0800

    add sort options, checkboxes and action buttons

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,71 +1,84 @@
-<!-- Fix this tomorrow -->
+<?php
+if (isset($_POST['checkboxArray'])) {
+    $checkboxArray = $_POST['checkboxArray'];
 
-<table id="post_table" class="table table-hover text-nowrap">
-    <thead>
-        <tr>
-            <th>Id</th>
-            <th>Author</th>
-            <th>Title</th>
-            <th>Category</th>
-            <th>Status</th>
-            <th>Image</th>
-            <th>Tags</th>
-            <th>Comments</th>
-            <th>Date</th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        $posts_row_id = 1;
+    foreach ($checkboxArray as $checkboxValue) {
+        echo $checkboxValue;
+    }
+}
 
-        $query = "SELECT * FROM posts";
-        $select_posts = mysqli_query($conn, $query);
-        while ($row = mysqli_fetch_assoc($select_posts)) {
-            $posts_id = $row['posts_id'];
-            $post_cat_id = $row['post_cat_id'];
-            $post_title = $row['post_title'];
-            $post_author = $row['post_author'];
-            $post_date = $row['post_date'];
-            $post_image = $row['post_image'];
-            $post_content = $row['post_content'];
-            $data = str_replace('&', '&amp;', $post_content);
-            $post_tag = $row['post_tag'];
-            $post_comment_count = $row['post_comment_count'];
-            $post_status = $row['post_status'];
 
-            $query_category = "SELECT * FROM categories WHERE category_id ={$post_cat_id}";
-            $fetch_result  = mysqli_query($conn, $query_category);
+?>
 
-            while ($row = mysqli_fetch_assoc($fetch_result)) {
-                $category_title = $row['category_title'];
-            }
 
-            echo "
-                <tr>
-                    <td>{$posts_row_id}</td>
-                    <td>{$post_author}</td>
-                    <td>{$post_title}</td>
-                    <td>{$category_title}</td>
-                    <td>{$post_status}</td>
-                    <td><img class='img-fluid' src='../images/$post_image'></td>
-                    <td>{$post_tag}</td>
-                    <td>{$post_comment_count}</td>
-                    <td>{$post_date}</td>
-                    <td>
-                        <a href='view-posts.php?delete={$posts_id}' class='btn btn-danger btn-sm rounded-circle'>
-                            <i class='fas fa-trash'></i>
-                        </a>
-                        <a href='view-posts.php?source=edit_post&p_id={$posts_id}' class='btn btn-success btn-sm edit_posts rounded-circle'>
-                            <i class='fas fa-edit'></i>
-                        </a>
-                    </td>
-                </tr>";
-            $posts_row_id++;
-        }
-        ?>
-    </tbody>
-</table>
+<form action="" method="POST">
+    <div class="container">
+        <div class="p-3 mb-1">
+            <div class="form-row">
+                <div class="col-md-12 mb-3">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <label class="col-form-label" for="">View by Status</label>
+                            <select name="post_status" id="post_status" class="form-control">
+                                <option value="view">View All</option>
+                                <option value="Published">Published</option>
+                                <option value="Draft">Draft</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4">
+                            <label class="col-form-label" for="">View by Category</label>
+                            <select name="post_status" id="post_status" class="form-control">
+                                <option value="view">View All</option>
+                                <option value="Published">Published</option>
+                                <option value="Draft">Draft</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4">
+                            <label class="col-form-label" for="">View by Author</label>
+                            <select name="post_status" id="post_status" class="form-control">
+                                <option value="view">View All</option>
+                                <option value="Published">Published</option>
+                                <option value="Draft">Draft</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <button class="btn btn-danger" type="submit"><i class="fas fa-trash"></i> Delete</button>
+                    <button class="btn btn-success" type="submit"><i class="fas fa-paste"></i> Publish</button>
+                    <button class="btn btn-primary" type="submit"><i class="fas fa-list"></i> Draft</button>
+                    <a href="view-posts.php?source=add_post" class="btn btn-info float-right" type="submit"><i
+                            class="fas fa-plus-circle"></i> Add new</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr class="divider">
+    <table id="post_table" class="table table-hover text-nowrap">
+        <thead>
+            <tr>
+                <th>
+                    <label class="custom-control material-checkbox">
+                        <input type="checkbox" class="material-control-input" id="selectAll">
+                        <span class="material-control-indicator"></span>
+                    </label>
+                </th>
+                <th>Id</th>
+                <th>Author</th>
+                <th>Title</th>
+                <th>Category</th>
+                <th>Status</th>
+                <th>Image</th>
+                <th>Tags</th>
+                <th>Comments</th>
+                <th>Date</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+</form>
 
 <?php
 if (isset($_GET['delete'])) {
