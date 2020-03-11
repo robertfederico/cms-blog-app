a785ae0d41d1823ce94c8da2347940e50a09b52e
robertfederico <robertbfederico@gmail.com>
2020-03-12 00:07:49 +0800

    add scriptsa and center footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,18 +1,19 @@
-   <footer>
-       <div class="row">
-           <div class="col-lg-12">
-               <p>Copyright &copy; Robert Blog <?php echo date('Y'); ?></p>
-           </div>
-       </div>
-   </footer>
+<footer>
+    <div class="row">
+        <div class="col-lg-12">
+            <p class="text-center">Copyright &copy; Robert Blog <?php echo date('Y'); ?></p>
+        </div>
+    </div>
+</footer>
+</div>
 
-   <!-- jQuery -->
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-       integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-   </script>
-   <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
-   <script src="js/login.js"></script>
-   </body>
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+</script>
+<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+<script src="js/login.js"></script>
+</body>
 
-   </html>
+</html>
