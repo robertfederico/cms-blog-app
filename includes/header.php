4418fac42ec13b28e33006a2454b7fda2a06adf6
robertfederico <robertbfederico@gmail.com>
2020-03-12 00:10:15 +0800

    add styles

--- includes/header.php
+++ includes/header.php
@@ -18,7 +18,126 @@
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/style.css">
 
-    <link rel="stylesheet" href=" https://use.fontawesome.com/releases/v5.0.13/css/all.css">
+    <!-- <link rel="stylesheet" href=" https://use.fontawesome.com/releases/v5.0.13/css/all.css"> -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-xVVam1KS4+Qt2OrFa+VdRUoXygyKIuNWUUUBZYv+n27STsJ7oDOHJgfF0bNKLMJF" crossorigin="anonymous">
+
+    <style>
+    #loader {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        z-index: 999;
+        width: 150px;
+        height: 150px;
+        margin: -75px 0 0 -75px;
+        border: 16px solid #f3f3f3;
+        border-radius: 50%;
+        border-top: 16px solid #3498db;
+        width: 120px;
+        height: 120px;
+        -webkit-animation: spin 2s linear infinite;
+        animation: spin 2s linear infinite;
+    }
+
+    @-webkit-keyframes spin {
+        0% {
+            -webkit-transform: rotate(0deg);
+        }
+
+        100% {
+            -webkit-transform: rotate(360deg);
+        }
+    }
+
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
+    /* Add animation to "page content" */
+    .animate-bottom {
+        position: relative;
+        -webkit-animation-name: animatebottom;
+        -webkit-animation-duration: 1s;
+        animation-name: animatebottom;
+        animation-duration: 1s
+    }
+
+    @-webkit-keyframes animatebottom {
+        from {
+            bottom: -100px;
+            opacity: 0
+        }
+
+        to {
+            bottom: 0px;
+            opacity: 1
+        }
+    }
+
+    @keyframes animatebottom {
+        from {
+            bottom: -100px;
+            opacity: 0
+        }
+
+        to {
+            bottom: 0;
+            opacity: 1
+        }
+    }
+
+    #myDiv {
+        display: none;
+    }
+
+    .alert_reg {
+        display: none;
+        padding: 20px;
+        background-color: #f44336;
+        color: white;
+        transition: opacity 0.6s;
+        border-radius: 5px;
+        margin-bottom: 10px;
+    }
+
+    .alert_log {
+        display: none;
+        padding: 20px;
+        background-color: #f44336;
+        color: white;
+        transition: opacity 0.6s;
+        border-radius: 5px;
+        margin-bottom: 10px;
+    }
+
+    .closebtn {
+        margin-left: 15px;
+        color: white;
+        font-weight: bold;
+        float: right;
+        font-size: 22px;
+        line-height: 20px;
+        cursor: pointer;
+        transition: all .5s;
+    }
+
+    .closebtn:hover {
+        color: black;
+    }
+
+    .modal-dialog {
+        width: 1000px !important;
+    }
+    </style>
 </head>
 
-<body>
+<body onload="myFunction()" style="margin:0;">
+    <div id="loader"></div>
+    <div style="display:none;" id="myDiv" class="animate-bottom">
