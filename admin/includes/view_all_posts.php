4a19891ea4d79873160d2762bf377684c717c3a8
robertfederico <robertbfederico@gmail.com>
2020-03-12 00:08:36 +0800

    add functions for each action buttons

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,15 +1,38 @@
 <?php
-if (isset($_POST['checkboxArray'])) {
+
+if (isset($_POST['publish'])) {
+    $checkboxArray = $_POST['checkboxArray'];
+    foreach ($checkboxArray as $post_value_id) {
+
+        $post_status = 'Published';
+
+        $query = "UPDATE posts SET post_status = '{$post_status}' WHERE posts_id = {$post_value_id}";
+        confirm_query($query);
+    }
+}
+if (isset($_POST['draft'])) {
     $checkboxArray = $_POST['checkboxArray'];
 
-    foreach ($checkboxArray as $checkboxValue) {
-        echo $checkboxValue;
+    foreach ($checkboxArray as $post_value_id) {
+
+        $post_status = 'Draft';
+
+        $query = "UPDATE posts SET post_status = '{$post_status}' WHERE posts_id = {$post_value_id}";
+        confirm_query($query);
     }
 }
 
+if (isset($_POST['delete'])) {
+    $checkboxArray = $_POST['checkboxArray'];
+
+    foreach ($checkboxArray as $post_value_id) {
+
+        $query = "DELETE FROM posts WHERE posts_id = {$post_value_id}";
+        confirm_query($query);
+    }
+}
 
 ?>
-
 
 <form action="" method="POST">
     <div class="container">
@@ -27,7 +50,7 @@ if (isset($_POST['checkboxArray'])) {
                         </div>
                         <div class="col-md-4">
                             <label class="col-form-label" for="">View by Category</label>
-                            <select name="post_status" id="post_status" class="form-control">
+                            <select name="post_status1" id="post_status" class="form-control">
                                 <option value="view">View All</option>
                                 <option value="Published">Published</option>
                                 <option value="Draft">Draft</option>
@@ -35,7 +58,7 @@ if (isset($_POST['checkboxArray'])) {
                         </div>
                         <div class="col-md-4">
                             <label class="col-form-label" for="">View by Author</label>
-                            <select name="post_status" id="post_status" class="form-control">
+                            <select name="post_status2" id="post_status" class="form-control">
                                 <option value="view">View All</option>
                                 <option value="Published">Published</option>
                                 <option value="Draft">Draft</option>
@@ -44,9 +67,12 @@ if (isset($_POST['checkboxArray'])) {
                     </div>
                 </div>
                 <div class="col-md-12">
-                    <button class="btn btn-danger" type="submit"><i class="fas fa-trash"></i> Delete</button>
-                    <button class="btn btn-success" type="submit"><i class="fas fa-paste"></i> Publish</button>
-                    <button class="btn btn-primary" type="submit"><i class="fas fa-list"></i> Draft</button>
+                    <button class="btn btn-danger" type="submit" name="delete"><i class="fas fa-trash"></i>
+                        Delete</button>
+                    <button class="btn btn-success" type="submit" name="publish"><i class="fas fa-paste"></i>
+                        Publish</button>
+                    <button class="btn btn-primary" type="submit" name="draft"><i class="fas fa-list"></i>
+                        Draft</button>
                     <a href="view-posts.php?source=add_post" class="btn btn-info float-right" type="submit"><i
                             class="fas fa-plus-circle"></i> Add new</a>
                 </div>
