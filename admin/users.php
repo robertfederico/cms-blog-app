406519cfed8f96388cf134341406c0f90249440d
robertfederico <robertbfederico@gmail.com>
2020-03-14 22:22:20 +0800

    update ui

--- admin/users.php
+++ admin/users.php
@@ -8,46 +8,39 @@ include 'includes/header.php';
     <!-- sidebar-wrapper  -->
     <main class="page-content">
         <div class="container-fluid">
+            <?php
+            if (isset($_GET['source'])) {
+                $source = $_GET['source'];
+            } else {
+                $source = '';
+            }
+            switch ($source) {
+                case 'add_post';
+                    echo '<h5 class="title-header">Add User</h5>';
+                    break;
+                case 'edit_user';
+                    echo '<h5 class="title-header">Edit User</h5>';
+                    break;
+                default:
+                    echo '<h5 class="title-header">Users</h5>';
+                    break;
+            }
+
+            ?>
             <div class="card shadow">
-                <div class="card-header bg-transparent">
-                    <?php
-                    if (isset($_GET['source'])) {
-                        $source = $_GET['source'];
-                    } else {
-                        $source = '';
-                    }
-
-                    switch ($source) {
-                        case 'add_post';
-                            echo '<h2>Add User</h2>';
-                            break;
-                        case 'edit_user';
-                            echo '<h2>Edit User</h2>';
-                            break;
-                        default:
-                            echo '<h2>Users</h2>';
-                            break;
-                    }
-
-                    ?>
-                </div>
-                <div class="card-body">
-                    <?php
-                    switch ($source) {
-                        case 'add_user';
-                            include 'includes/add_user.php';
-                            break;
-                        case 'edit_user';
-                            include 'includes/edit_user.php';
-                            break;
-                        default:
-                            echo ' <div class="table-responsive">';
-                            include 'includes/view_all_users.php';
-                            echo '</div>';
-                            break;
-                    }
-                    ?>
-                </div>
+                <?php
+                switch ($source) {
+                    case 'add_user';
+                        include 'includes/add_user.php';
+                        break;
+                    case 'edit_user';
+                        include 'includes/edit_user.php';
+                        break;
+                    default:
+                        include 'includes/view_all_users.php';
+                        break;
+                }
+                ?>
             </div>
         </div>
     </main>
