6b342b83d69c7ba512c729a30036bc00f832d3c0
robertfederico <robertbfederico@gmail.com>
2020-03-14 22:21:26 +0800

    update ui

--- admin/categories.php
+++ admin/categories.php
@@ -7,34 +7,19 @@ include 'includes/header.php';
     <?php include 'includes/sidenav.php'; ?>
     <!-- sidebar-wrapper  -->
     <main class="page-content">
-        <div class="container-fluid">
-            <h2>Categories</h2>
-            <hr>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="card shadow p-4">
-                        <!-- save vategories -->
-                        <?php
-                        insert_categories();
-                        ?>
-                        <form action="" method="POST" enctype="multipart/form-data">
-                            <div class="form-group">
-                                <label for="cat_title">Category Title</label>
-                                <input name="cat_title" type="text" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="cat_image">Category Image</label>
-                                <input name="cat_image" type="file" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <button class="btn btn-primary" name="submit" type="submit">Add Category</button>
-                            </div>
-                        </form>
+        <h5 class="title-header">Categories</h5>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="bg-white">
+                    <div class="title-container">
+                        <h5>Categories</h5>
+                        <p>Here you can manage categories</p>
                     </div>
-                </div>
-
-                <div class="col-md-8">
-                    <div class="card shadow p-4">
+                    <div class="filter-container">
+                        <button href='#' class='btn btn-success custom-btn-primary mb-3' data-toggle="modal"
+                            data-target="#add_modal">
+                            Add Category
+                        </button>
                         <div class="table-responsive">
                             <table id="categories" class="table table-hover text-nowrap">
                                 <thead>
@@ -64,6 +49,43 @@ include 'includes/header.php';
         </div>
     </main>
 </div>
+<!-- Add  Modal -->
+<div class="modal fade" id="add_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add Category</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <!-- save vategories -->
+                <?php
+                insert_categories();
+                ?>
+                <form action="" method="POST" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="cat_title">Category Title</label>
+                        <input name="cat_title" type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="cat_image">Category Image</label>
+                        <div class="card border-dark p-1 mb-2">
+                            <img id="img" src="../images/upload.png" class="img-fluid" alt="image">
+                        </div>
+                        <input name="cat_image" id="update_post_image" type="file" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-primary custom-btn-primary" name="submit" type="submit">Add
+                            Category</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 
 <!-- Modal -->
 <div class="modal fade" id="edit_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
@@ -88,7 +110,10 @@ include 'includes/header.php';
                     </div>
                     <div class="form-group">
                         <label for="vat_image">Category Image</label>
-                        <input type="file" class="form-control" name="update_cat_image">
+                        <div class="card border-dark p-1 mb-2">
+                            <img id="image" src="" id="image" class="img-fluid" alt="image">
+                        </div>
+                        <input type="file" id="cat_file" class="form-control" name="update_cat_image">
                     </div>
                     <div class="form-group">
                         <input name="update_category_id" id="update_category_id" type="hidden">
