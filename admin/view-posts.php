4e2ab481ba98ef02ea06afcc5a273fa4b152b08e
robertfederico <robertbfederico@gmail.com>
2020-03-14 22:22:30 +0800

    update ui

--- admin/view-posts.php
+++ admin/view-posts.php
@@ -8,44 +8,37 @@ include 'includes/header.php';
     <!-- sidebar-wrapper  -->
     <main class="page-content">
         <div class="container-fluid">
+            <?php
+            if (isset($_GET['source'])) {
+                $source = $_GET['source'];
+            } else {
+                $source = '';
+            }
+
+            if ($source == 'add_post') {
+                echo '<h5 class="title-header">Add Post</h5>';
+            } else if ($source == 'edit_post') {
+
+                echo '<h5 class="title-header">Edit Post</h5>';
+            } else {
+
+                echo '<h5 class="title-header">Posts</h5>';
+            }
+            ?>
             <div class="card shadow">
-                <div class="card-header bg-transparent">
-                    <?php
-                    if (isset($_GET['source'])) {
-                        $source = $_GET['source'];
-                    } else {
-                        $source = '';
-                    }
-
-                    if ($source == 'add_post') {
-
-                        echo '<h2>Add Post</h2>';
-                    } else if ($source == 'edit_post') {
-
-                        echo '<h2>Edit Post</h2>';
-                    } else {
-
-                        echo '<h2>Post</h2>';
-                    }
-                    ?>
-                </div>
-                <div class="card-body">
-                    <?php
-                    switch ($source) {
-                        case 'add_post';
-                            include 'includes/add_posts.php';
-                            break;
-                        case 'edit_post';
-                            include 'includes/edit_post.php';
-                            break;
-                        default:
-                            echo ' <div class="table-responsive">';
-                            include 'includes/view_all_posts.php';
-                            echo '</div>';
-                            break;
-                    }
-                    ?>
-                </div>
+                <?php
+                switch ($source) {
+                    case 'add_post';
+                        include 'includes/add_posts.php';
+                        break;
+                    case 'edit_post';
+                        include 'includes/edit_post.php';
+                        break;
+                    default:
+                        include 'includes/view_all_posts.php';
+                        break;
+                }
+                ?>
             </div>
         </div>
     </main>
@@ -56,4 +49,21 @@ include 'includes/header.php';
 <?php
 update_category();
 ?>
+<script src="https://cdn.ckeditor.com/ckeditor5/17.0.0/classic/ckeditor.js"></script>
+<script>
+let editor;
+
+ClassicEditor
+    .create(document.querySelector('#post_content'))
+    .then(editor => {
+        console.log(editor);
+    })
+
+ClassicEditor
+    .create(document.querySelector('#update_post_content'))
+    .then(newEditor => {
+        editor = newEditor;
+    })
+</script>
+
 <?php include 'includes/footer.php'; ?>
